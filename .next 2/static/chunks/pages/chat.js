e1e6f485228438bec25a1bb78555e091197d8867
beknazar93 <bekishsuper@gmail.com>
2025-01-05 19:52:25 +0600

    2
    2

--- .next 2/static/chunks/pages/chat.js
+++
@@ -1,8 +0,0 @@
-__turbopack_load_page_chunks__("/chat", [
-  "static/chunks/node_modules_next_dist_f1b02b._.js",
-  "static/chunks/node_modules_react-dom_82bb97._.js",
-  "static/chunks/node_modules_975914._.js",
-  "static/chunks/[root of the server]__8a13bb._.js",
-  "static/chunks/src_pages_index_5771e1._.js",
-  "static/chunks/src_pages_index_403134._.js"
-])
